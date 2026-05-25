ecc14f920529f059905f6fd8e3d655b38a6405f3
Shubham <shubhamsingh705010@gmail.com>
2026-05-25 19:24:21 +0530

    data types done

---
+++ 01_BASICS/02_datatypes.js
@@ -0,0 +1,9 @@
+// alert(3 + 3) // its an error coz we are using nodejs, not browser
+
+let name = "shubh" //string
+let age = 18  //number
+let isLoggedIn = false  //boolean yes or no
+
+//null => standalone value which means noting / not zero
+// undefined => no value has been assigned
+
