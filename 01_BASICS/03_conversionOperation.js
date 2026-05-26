919844be50e09166a8706d557df5258ba66d630f
Shubham <shubhamsingh705010@gmail.com>
2026-05-26 10:02:12 +0530

    conversion

--- 01_BASICS/03_conversionOperation.js
+++ 01_BASICS/03_conversionOperation.js
@@ -13,5 +13,27 @@ console.log(valueInNumber);
 // "33abc" => NaN
 // true => 1 false => 0
 
+// ********************************OPERATIONS****************************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(5 + 7);
+console.log(8 -9);
+console.log(2*4);
+console.log(3**9);
+console.log(3/6);
+
+let str1 = "hello"
+let str2 = "shubh"
+let str3 = str1 + str2
+console.log(str3);
+
+
+
+
+
+
 
 
