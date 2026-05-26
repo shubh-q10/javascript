c3db4b6cc742afacb014e4a0b2fd7ca7ee9a5705
Shubham <shubhamsingh705010@gmail.com>
2026-05-26 13:37:05 +0530

    nums and math complete

--- 01_BASICS/06_nums_and_maths.js
+++ 01_BASICS/06_nums_and_maths.js
@@ -20,3 +20,26 @@ const hundreds = 1000000
 console.log(hundreds.toLocaleString()); // by default this put commas according to american system
 console.log(hundreds.toLocaleString('en-IN')); //this put commas according to indian system
 
+//********************************** MATHS ***************************************** */
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(5.6)); //round off to nearest integer
+console.log(Math.ceil(4.1)); //always give next highest integer
+console.log(Math.floor(5.6)); //always give near lowest integer
+console.log(Math.sqrt(49));
+
+
+console.log(Math.random()); //this give random numbers between 0 and 1
+console.log((Math.random()*10) + 1); //this give random numbers between 1 and 10
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+
+
+
+
+
