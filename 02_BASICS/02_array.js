157148971c02bda2de5b2656611a636013f68576
Shubham <shubhamsingh705010@gmail.com>
2026-05-27 20:57:47 +0530

    arrays done

--- 02_BASICS/02_array.js
+++ 02_BASICS/02_array.js
@@ -28,6 +28,21 @@ const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 const real_another_array = another_array.flat(Infinity) // it spread out all the levels of arrays and give a new array with all the elements
 console.log(real_another_array);
 
+console.log(Array.isArray("hitesh"));
+console.log(Array.from("hitesh")); // it converts the value into an array like here it will give ["h", "i", "t", "e", "s", "h"]
+console.log(Array.from({name: "hitesh"}));
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3)); // it takes value of diff variables and put all the values into an array
+
+
+
+
+
 
 
 
