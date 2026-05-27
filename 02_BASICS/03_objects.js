b877e2f8faac7064fd6af22bcff7378c2596da81
Shubham <shubhamsingh705010@gmail.com>
2026-05-27 21:35:40 +0530

    object part 2

--- 02_BASICS/03_objects.js
+++ 02_BASICS/03_objects.js
@@ -1,40 +1,34 @@
 //singleton: when we make objects through constructor method
 
 //object literals: when we make objects through curly braces {}
-const mySym = Symbol("key1")
+const mySym = Symbol("key1");
 
 const jsuser = {
-    name: "shubh",
-    "fullname": "shubham singh",
-    [mySym]: "mykey1",
-    age: 22,
-    location: "jaipur",
-    email: "hitesh@google.com",
-    isLoggedIn: false,
-    lastLoginDays: ["Monday", "Saturday"]
-}
+  name: "shubh",
+  fullname: "shubham singh",
+  [mySym]: "mykey1",
+  age: 22,
+  location: "jaipur",
+  email: "hitesh@google.com",
+  isLoggedIn: false,
+  lastLoginDays: ["Monday", "Saturday"],
+};
 
-jsuser.email = "shubh@chatgpt.com"
+jsuser.email = "shubh@chatgpt.com";
 // Object.freeze(jsuser) // here i have freezed the object jsuser so that its values cannot be changed
-jsuser.email = "checking@gmail.com" // here email didnot changed coz i have freezed the object
+jsuser.email = "checking@gmail.com"; // here email didnot changed coz i have freezed the object
 
 console.log(jsuser.email);
 console.log(jsuser["email"]);
 console.log(jsuser["fullname"]);
 console.log(jsuser[mySym]);
 
-
 jsuser.greeting = function () {
-    console.log("Hello js user");
-}
-
-
+  console.log("Hello js user");
+};
 
 jsuser.greetingTwo = function () {
-    console.log(`Hello js user, ${this.name}`);
-    
-    
-}
+  console.log(`Hello js user, ${this.name}`);
+};
 console.log(jsuser.greeting());
 console.log(jsuser.greetingTwo());
-
---
+++ 02_BASICS/04_object.js
@@ -0,0 +1,11 @@
+//we have two methods to write the object which is singleton and non-singleton(literal)
+
+// const twitterUser = new Object() // THIS IS SINGLETON OBJECT
+const tinderUser = {}  // THIS IS LITERAL OBJECT
+
+tinderUser.id = "345txc"
+tinderUser.name = "shubh"
+tinderUser.isLoggedIN = false
+
+
+console.log(tinderUser);
