5810785037591fa821ec0cadd3367ab744dc8960
Shubham <shubhamsingh705010@gmail.com>
2026-05-29 20:57:26 +0530

    functions 50 percent done

--- 03_BASICS/01_functions.js
+++ 03_BASICS/01_functions.js
@@ -18,3 +18,19 @@ function addTwoNumber(num1, num2) {
 }
 
 console.log(addTwoNumber(89, 90));
+
+//here default value is "sam" in case user don't give input value
+
+function loginUserMessage(username = "sam"){
+    if(username === undefined){
+        console.log("Please enter a username");
+        return
+        
+
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("ankit"));
+console.log(loginUserMessage()); // here we have not given any input but default is "sam"
+
