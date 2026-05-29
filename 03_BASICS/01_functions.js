a64de7664203118896874c84d3da78464674f6eb
Shubham <shubhamsingh705010@gmail.com>
2026-05-29 20:45:36 +0530

    function

---
+++ 03_BASICS/01_functions.js
@@ -0,0 +1,20 @@
+//function: A function in JavaScript is a reusable block of code that performs a specific task.
+
+function sayMyName() {
+    console.log("S");
+    console.log("H");
+    console.log("U");
+    console.log("B");
+    console.log("H");
+}
+
+sayMyName()
+
+
+function addTwoNumber(num1, num2) {
+    let result = num1 + num2
+    return result
+    
+}
+
+console.log(addTwoNumber(89, 90));
