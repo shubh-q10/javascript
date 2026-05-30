0facf01d5d7aa3fbb158769bc642c35faf198f50
Shubham <shubhamsingh705010@gmail.com>
2026-05-30 18:53:51 +0530

    functions

--- 03_BASICS/01_functions.js
+++ 03_BASICS/01_functions.js
@@ -1,36 +1,31 @@
 //function: A function in JavaScript is a reusable block of code that performs a specific task.
 
 function sayMyName() {
-    console.log("S");
-    console.log("H");
-    console.log("U");
-    console.log("B");
-    console.log("H");
+  console.log("S");
+  console.log("H");
+  console.log("U");
+  console.log("B");
+  console.log("H");
 }
 
-sayMyName()
-
+sayMyName();
 
 function addTwoNumber(num1, num2) {
-    let result = num1 + num2
-    return result
-    
+  let result = num1 + num2;
+  return result;
 }
 
 console.log(addTwoNumber(89, 90));
 
 //here default value is "sam" in case user don't give input value
 
-function loginUserMessage(username = "sam"){
-    if(username === undefined){
-        console.log("Please enter a username");
-        return
-        
-
-    }
-    return `${username} just logged in`
+function loginUserMessage(username = "sam") {
+  if (username === undefined) {
+    console.log("Please enter a username");
+    return;
+  }
+  return `${username} just logged in`;
 }
 
 console.log(loginUserMessage("ankit"));
 console.log(loginUserMessage()); // here we have not given any input but default is "sam"
-
