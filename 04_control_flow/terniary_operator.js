1dd1060923cf70ed3a7cdd07fa68e2f8ccc1b06b
Shubham <shubhamsingh705010@gmail.com>
2026-05-31 14:01:39 +0530

    control flow

--- 04_control_flow/terniary_operator.js
+++ 04_control_flow/terniary_operator.js
@@ -4,5 +4,5 @@
 
 const iceTeaPrice = 100
 
-iceTeaPrice >= 80 ? console.log("Price is less than 80"): console.log("Price is more than 80");
+iceTeaPrice <= 80 ? console.log("Price is less than 80"): console.log("Price is more than 80");
 
