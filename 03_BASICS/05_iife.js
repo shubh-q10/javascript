b72a3b1e23a6ebb6a90ab5288da1d63086284f3d
Shubham <shubhamsingh705010@gmail.com>
2026-05-31 08:04:12 +0530

    iife done

--- 03_BASICS/05_iife.js
+++ 03_BASICS/05_iife.js
@@ -3,10 +3,23 @@
 // PUT THE FUNCTION INSIDE A PARENTHESIS AND ONE PARENTHESIS JUST AFTER THAT TO IMMEDIETLY RUN THE FUNCTION RIGHT AWAY
 
 (function one() {
-    console.log(`DB CONNECTED`);
+  console.log(`DB CONNECTED`);
+})();
+
+
+// but if you write two IIFE function just after each other then you need to stop the first function using ; otherwise it will give an error
+
+
+// arrow function
+
+(() => {
+    console.log(`DB CONNECTED THROUGH ARROW FUNCTION`);
     
-})()
+})();
 
+// passing variable in IIFE function
 
-
-
+((name) => {
+    console.log(`hello ${name}, welcome to IIFE function `);
+    
+})("Shubham Singh")
