b52dc9ee4c8bc114cbc616f1b815d766abdeb794
Shubham <shubhamsingh705010@gmail.com>
2026-05-31 07:47:56 +0530

    iife

--- 03_BASICS/05_iife.js
+++ 03_BASICS/05_iife.js
@@ -1,1 +1,12 @@
 // IIFE: Immediately Invoked Function Expressions (IIFE)
+
+// PUT THE FUNCTION INSIDE A PARENTHESIS AND ONE PARENTHESIS JUST AFTER THAT TO IMMEDIETLY RUN THE FUNCTION RIGHT AWAY
+
+(function one() {
+    console.log(`DB CONNECTED`);
+    
+})()
+
+
+
+
