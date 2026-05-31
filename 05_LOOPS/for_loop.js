83ca9d73ee73c56a9929a8adf4179012693f383a
Shubham <shubhamsingh705010@gmail.com>
2026-05-31 14:21:39 +0530

    loop

--- 05_LOOPS/for_loop.js
+++ 05_LOOPS/for_loop.js
@@ -1,8 +1,19 @@
 // FOR LOOP
 
-for (let index = 0; index < 10; index++) {
-    const element = index;
-    console.log(element);
-    
-    
+for (let i = 0; i < 10; i++) {
+  const element = i;
+  if (element == 5) {
+    console.log("5 is the best number");
+  }
+  console.log(element);
+}
+
+
+
+for (let i = 0; i <= 10; i++) {
+  console.log(`Outer loop value: ${i}`);
+
+  for (let j = 0; j <= 10; j++) {
+    console.log(`Inner loop value: ${j} and Outer loop ${i}`);
+  }
 }
