555a31d53640b2dc8fa4798ac7989137ba3fb4eb
Shubham <shubhamsingh705010@gmail.com>
2026-05-31 14:29:43 +0530

    for loop table

--- 05_LOOPS/for_loop.js
+++ 05_LOOPS/for_loop.js
@@ -10,12 +10,24 @@ for (let i = 0; i < 10; i++) {
 
 
 
-for (let i = 0; i <= 10; i++) {
+for (let i = 1; i <= 10; i++) {
   console.log(`Outer loop value: ${i}`);
 
-  for (let j = 0; j <= 10; j++) {
-    console.log(`Inner loop value: ${j} and Outer loop ${i}`);
+  for (let j = 1; j <= 10; j++) {
+    console.log(` Outer loop ${i} and Inner loop value: ${j}`);
+    
   }
 }
 
+
+// WRITING TABLE USING FOR LOOP
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`Multiplication table of: ${i}`);
+
+  for (let j = 1; j <= 10; j++) {
+    console.log(i + " X " + j + ' = ' + i*j);
+    
+  }
+}
 
