06a4bf59213a407c09e6d436474ca4c8d03e15ee
Shubham <shubhamsingh705010@gmail.com>
2026-05-31 06:53:45 +0530

    function

--- 03_BASICS/03_scopes.js
+++ 03_BASICS/03_scopes.js
@@ -1,4 +1,4 @@
-const { use } = require("react");
+
 
 let a = 690
 const b = 890
@@ -23,7 +23,7 @@ function one() {
         
         
     }
-    console.log(website);
+    // console.log(website);
     two()
     
     
@@ -40,21 +40,34 @@ if (true) {
         const website = "youtube"
         
     }
-    console.log(website);
+    // console.log(website);
     
     
 }
 
-console.log(username);
+// console.log(username);
 
 
 //++++++++++++++++++++++++++++interesting++++++++++++++++++++++++++++
+
+// two ways to write a function
+
+
+addone(5) // here we are calling function before declaring and this will work fine
 
 function addone(num) {
     return num + 1
 
 }
 
-addone(5)
+
+
+addTwo(8) // here also we are calling function before declaring but it will give an error and this is the difference between the two writing methods of function
+
+const addTwo = function(num) {
+    return num + 2
+
+}
+
 
 
