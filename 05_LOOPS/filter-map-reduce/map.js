250daaae6bf29473f08c906cb893e373186d0e7e
Shubham <shubhamsingh705010@gmail.com>
2026-06-03 16:37:51 +0530

    map

---
+++ 05_LOOPS/filter-map-reduce/map.js
@@ -0,0 +1,8 @@
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// adding 10 to every number in myNumbers array
+
+const addednums = myNumbers.map((num) => num + 10)
+
+console.log(addednums);
+
