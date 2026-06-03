fee850fe3a5a83ae1b7d5c9bc7a537c4a2e6de85
Shubham <shubhamsingh705010@gmail.com>
2026-06-03 10:38:21 +0530

    object inside the array accessed

--- 05_LOOPS/arrays_specific_loops/for-each_loop.js
+++ 05_LOOPS/arrays_specific_loops/for-each_loop.js
@@ -5,3 +5,47 @@ coding.forEach( (item) => {
     
 
 } )
+
+// we can put another function in foreach also
+
+function printme(anything) {
+    console.log(`Printed ${anything}`);
+    
+
+}
+
+// now we can put printme function in foreach
+
+coding.forEach(printme); // here you just have to give reference of the peintme function , you don't have to execute
+
+
+
+coding.forEach((item, index, arr) => {
+    console.log(item, index, arr);
+    
+})
+
+
+// objects inside the array
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+        },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    }
+]
+
+myCoding.forEach((item) => {
+    console.log(item.languageName);
+    
+
+})
+
