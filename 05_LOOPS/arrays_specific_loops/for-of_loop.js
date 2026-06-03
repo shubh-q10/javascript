dd9fe20f7a4130fe5d3a4d485d8c1bf3a5318991
Shubham <shubhamsingh705010@gmail.com>
2026-06-03 09:26:23 +0530

    for-of loop with continue

--- 05_LOOPS/arrays_specific_loops/for-of_loop.js
+++ 05_LOOPS/arrays_specific_loops/for-of_loop.js
@@ -1,7 +1,18 @@
 // for of loop
 
+// for-of loop on array
 const arr = [1, 2, 3, 4, 5];
 
 for (const num of arr) {
   console.log(`Value of num after adding 3 is ${num + 3}`);
+}
+
+// for-of loop on string
+const greetings = "Hello World";
+
+for (const greet of greetings) {
+  if (greet == " ") {
+    continue;
+  }
+  console.log(`each car is ${greet}`);
 }
