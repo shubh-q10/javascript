0066b9cefd423f81b578c7da8c57e768a6c6de72
Shubham <shubhamsingh705010@gmail.com>
2026-06-03 13:04:38 +0530

    filter map reduce

--- 05_LOOPS/filter_map_reduce.js
+++ 05_LOOPS/filter_map_reduce.js
@@ -1,24 +1,31 @@
-const myNums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // filter
 
-const newNums = myNums.filter((num) => num > 4)
+const newNums = myNums.filter((num) => num > 4);
 
 // this will print empty array coz we are using {} but we are not returning anything
 
 const newNums0 = myNums.filter((num) => {
-    num > 4
-})
+  num > 4;
+});
 
 const newNums1 = myNums.filter((num) => {
-    return num > 4
-})
+  return num > 4;
+});
 
 console.log(newNums);
 console.log(newNums0);
 console.log(newNums1);
 
+// we can do the same above thing using forEach
 
+const newNum2 = [];
 
-//
+myNums.forEach((num) => {
+  if (num > 4) {
+    newNum2.push(num);
+  }
+});
+
+console.log(newNum2);
