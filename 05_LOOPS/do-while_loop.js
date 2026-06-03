d58378918ea3ec100fd4408304885c5ca6e25729
Shubham <shubhamsingh705010@gmail.com>
2026-06-03 09:13:54 +0530

    do while done

--- 05_LOOPS/do-while_loop.js
+++ 05_LOOPS/do-while_loop.js
@@ -1,6 +1,23 @@
 
 // do-while loop
 
+
+let score = 1
+
 do {
+    console.log(`Score is ${score}`);
+    score++
     
-} while (condition);
+    
+} while (score <= 10);
+
+// what if score value is already greater than 10
+// it will print 11 coz in do-while loop do happens first and condition happen later
+
+let score1 = 11
+do {
+    console.log(`Score1 is ${score1}`);
+    score1++
+    
+    
+} while (score1 <= 10);
