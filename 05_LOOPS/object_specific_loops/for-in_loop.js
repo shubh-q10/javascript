b1a23b196b949241033e9778125b1e09452bab27
Shubham <shubhamsingh705010@gmail.com>
2026-06-03 10:02:50 +0530

    for in loop

---
+++ 05_LOOPS/object_specific_loops/for-in_loop.js
@@ -0,0 +1,16 @@
+const myobject = {
+    py: "python", 
+    js: "javascript",
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+
+//for-in loop
+
+for (const key in myobject) {
+    console.log(`${key} is shortcut for ${myobject[key]}`);
+    
+    
+    
+}
