63054f9be9e67a36e7e3ccf9c02e22f98e498d3f
Shubham <shubhamsingh705010@gmail.com>
2026-06-03 09:33:53 +0530

    map

--- 05_LOOPS/maps.js
+++ 05_LOOPS/maps.js
@@ -1,6 +1,10 @@
-//Maps
+//Maps: it is for unique values only
 
 const map = new Map
 map.set("IN", "India")
 map.set("USA", "United States of America")
 map.set("Fr", "France")
+map.set("IN", "India") // i have added india two times but it will print only once in map
+
+console.log(map);
+
