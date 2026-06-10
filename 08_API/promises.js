748e0200a22e851d6c84891341dd49de56d1b420
Shubham <shubhamsingh705010@gmail.com>
2026-06-10 18:35:01 +0530

    dot then

--- 08_API/promises.js
+++ 08_API/promises.js
@@ -1,3 +1,26 @@
-const promiseOne = new Promise(function() {
-    
-})
+const promiseOne = new Promise(function (resolve, reject) {
+  // do an async task
+  // DB calls, cryptography,
+
+  setTimeout(function () {
+    console.log("Async task is completed");
+    resolve();
+  }, 1000);
+});
+
+promiseOne.then(function () {
+  console.log("Promise consumed");
+});
+
+// this time we will not keep promise in variable and apply .then directly
+
+new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("Async task two");
+    resolve();
+  }, 1000);
+}).then(function () {
+  console.log("Async 2 resolved");
+});
+
+
