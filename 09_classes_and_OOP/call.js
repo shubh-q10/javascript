67aae2c8e6db5b73e05aa263ecf3a9b75a316be9
Shubham <shubhamsingh705010@gmail.com>
2026-06-12 17:03:23 +0530

    call

---
+++ 09_classes_and_OOP/call.js
@@ -0,0 +1,15 @@
+function SetUsername(username) {
+  this.username = username;
+}
+
+function createUser(username, email, password) {
+    SetUsername.call(this, username) 
+
+    this.email = email
+    this.password = password
+
+}
+
+const user1 = new createUser("shubh", 'shubh@gmail.com', "123dghie")
+
+console.log(user1);
