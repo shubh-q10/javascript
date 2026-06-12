ca859a7c0fc077413ff33b1df9426c2fe88fbf24
Shubham <shubhamsingh705010@gmail.com>
2026-06-12 16:39:38 +0530

    inheritence

--- 09_classes_and_OOP/inheritance.js
+++ 09_classes_and_OOP/inheritance.js
@@ -1,26 +1,43 @@
 const user = {
-    username: 'shubh',
-    email: "shubh@gmail.com"
-
-}
+  username: "shubh",
+  email: "shubh@gmail.com",
+};
 
 const teacher = {
-    makeVideo: true
-}
+  makeVideo: true,
+};
 
 const teachingSupport = {
-    isAvailable: false
-}
+  isAvailable: false,
+};
 
 const TAsupport = {
-    makeAssignment: 'js assignment',
-    fullTime: true,
-    __proto__: teachingSupport  // here TAsupport cam access the properties of teachingSupport also
-}
+  makeAssignment: "js assignment",
+  fullTime: true,
+  __proto__: teachingSupport, // here TAsupport cam access the properties of teachingSupport also
+};
 
-teacher.__proto__ = user  // here teacher can access the properties of user also
+teacher.__proto__ = user; // here teacher can access the properties of user also
 
 // MODERN SYNTAX
 
-Object.setPrototypeOf(teachingSupport, teacher) // here teachingSupport can access the properties of teacher
+Object.setPrototypeOf(teachingSupport, teacher); // here teachingSupport can access the properties of teacher
+
+// here im making a method called trueLength which trim the string
+
+let user1 = "shubh     ";
+
+String.prototype.trueLength = function () {
+  console.log(`${this}`);
+  console.log(`true lenght of text is: ${this.trim().length}`);
+};
+
+
+// NOW WE CAN USE THIS METHOD ON ANY STRING
+// here trueLength method is removing the whitespaces and then telling the length of the string
+
+user1.trueLength()
+'hiteshchoudhary     '.trueLength()
+"icetea".trueLength()
+
 
